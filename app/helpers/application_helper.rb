20c7ee71a4035d6a3be36a9f96ec280129e1c3b1
shako502 <tskhadadze.shako@gmail.com>
2021-08-12 15:17:33 +0400

    Add And Configure NavBar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
 end
+
+def nav_bar
+    content_tag(:ul, class: "navbar-nav me-auto mb-2 mb-lg-0") do
+        yield
+    end
+end
+
+def nav_link(text, path)
+    options = current_page?(path) ? { class: "nav-link active" } : {class: "nav-link"}
+    content_tag(:li, class: "nav-item") do
+      link_to text, path, options
+    end
+end
